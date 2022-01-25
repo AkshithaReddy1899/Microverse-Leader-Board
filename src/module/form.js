92b07b2497aa40fab13a006b84d0ef62e4cbde6a
AkshithaReddy1899 <akshithareddy1899@gmail.com>
2022-01-25 18:05:22 +0530

    add header to form

--- src/module/form.js
+++ src/module/form.js
@@ -1,5 +1,6 @@
 const Form = () => `<form id="form" class="row-md-3 row-sd-12">
-  <input type="text" class="input-name" placeholder="Your name">
+<h2 class="list-header">Add your score</h2>  
+<input type="text" class="input-name" placeholder="Your name">
   <input type="text" class="inut-score" placeholder="Your score">
   <button type="submit" class="btn btn-light" id="submit">Submit</button>
 </form>`;
