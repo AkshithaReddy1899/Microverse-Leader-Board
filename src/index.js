8909e40656bf92798ec09fda4a6b7650913141e1
AkshithaReddy1899 <akshithareddy1899@gmail.com>
2022-01-25 14:45:17 +0530

    import and export js components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './style.css';
+import List from './module/list.js';
+import Form from './module/form.js';
 
-console.log('OM')
+const container = document.getElementById('container');
+
+window.onload = () => {
+  container.innerHTML = List() + Form();
+};
---
+++ src/module/form.js
@@ -0,0 +1,7 @@
+const Form = () => `<form id="form" class="row-md-3 row-sd-12">
+  <input type="text" class="input-name" placeholder="Your name">
+  <input type="text" class="inut-score" placeholder="Your score">
+  <button type="submit" class="btn btn-light" id="submit">Submit</button>
+</form>`;
+
+export default Form;
